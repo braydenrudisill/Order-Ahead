8223ff2ae60ad9feec554ad79871d65a65e11600
Brayden Rudisill <braydenrudisill@gmail.com>
2021-08-29 12:45:20 -0700

    Initial Commit

---
+++ Order Ahead/Order_AheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Order_AheadApp.swift
+//  Order Ahead
+//
+//  Created by Brayden Rudisill on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Order_AheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
